075cc3a9f4dd460a63294b2e8fefeab5a2a96780
Thomas Dufourd <thomas.dufourd@bekk.no>
2013-11-07 16:18:09 +0100

    [examples] import examples som et prosjekt i IntelliJ

---
+++ examples/src/records_example.erl
@@ -0,0 +1,13 @@
+-module(records_example).
+
+-record(cat, {name, color=red, details=[]}).
+
+
+make_cat() -> #cat{name = "Bob", color=green}.
+
+
+%%inspect_cat(Cat = #cat{name=Name}) -> 	% pattern-matching record and field
+	%%...
+	%%Cat#cat.color 	% retrieve a field from the record
+	%%...
+	
