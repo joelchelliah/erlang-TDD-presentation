8eac9ed30a8f42148469cd0822d7b8442b5c241f
Thomas Dufourd <thomas.dufourd@bekk.no>
2013-11-07 21:47:36 +0100

    [getting ready] slett noen filer

--- tenniskata/coding_dojo_8_11/src/tennis_rules.erl
+++
@@ -1,7 +0,0 @@
--module(tennis_rules).
-
-%% API
--export([smoke/0]).
-
-smoke() ->
-  ok.
--- tenniskata/coding_dojo_8_11/src/tennis_rules_test.erl
+++
@@ -1,6 +0,0 @@
--module(tennis_rules_test).
-
--include_lib("eunit/include/eunit.hrl").
-
-a_first_test() ->
-  ?assertEqual(ok, tennis_rules:smoke()).
